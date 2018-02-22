8f580a584cbc192bda438389442150c9ed29e0bc
_asdx_ <xasdx@tuta.io>
2018-02-22 17:50:17 +0000

    Implement component management

--- src/processor.js
+++ src/processor.js
@@ -1,9 +1,25 @@
+let instances = []
+let wiringQueue = []
+
 function Component(target, name, descriptor) {
   console.log(`Found @Component on type ${target.name}`)
+  createInstance(target)
 }
 
 function Wired(target, name, descriptor) {
   console.log(`Found @Wired on field ${name}`)
+  wiringQueue.push({ target: target, name: name, descriptor: descriptor })
 }
 
-export { Component, Wired }
+function createInstance(target) {
+  let instanceEntry = { id: target.name, instance: new target() }
+  instances.push(instanceEntry)
+}
+
+function findInstance(id) {
+  let instanceEntry = instances.filter(i => i.id === id)[0]
+  if (!instanceEntry) { throw `Could not find matching instance for ${id}` }
+  return instanceEntry.instance
+}
+
+export { Component, Wired, findInstance }
